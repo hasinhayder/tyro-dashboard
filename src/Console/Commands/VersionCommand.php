15deca78dc6abf30c0b698f624f75c8cf4cdb8d9
Hasin Hayder <countdraculla@gmail.com>
2026-01-23 00:50:19 +0600

    Version 1.4.1

--- src/Console/Commands/VersionCommand.php
+++ src/Console/Commands/VersionCommand.php
@@ -21,7 +21,7 @@ class VersionCommand extends Command
      */
     public function handle(): int
     {
-        $version = "1.4.0";
+        $version = "1.4.1";
         
         $this->info('');
         $this->info('  ╔════════════════════════════════════════╗');
