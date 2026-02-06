f23ae0ecacf11108956f55f6dfbd413bcc54da7d
Hasin Hayder <countdraculla@gmail.com>
2026-02-06 10:26:56 +0530

    v1.8.0 - Invitation/Referral and Modal

--- src/Console/Commands/VersionCommand.php
+++ src/Console/Commands/VersionCommand.php
@@ -21,7 +21,7 @@ class VersionCommand extends Command
      */
     public function handle(): int
     {
-        $version = "1.8.0"; //Invitation/Referral System Addition
+        $version = "1.8.0"; //Invitation/Referral System Addition + Modals
         
         $this->info('');
         $this->info('  ╔════════════════════════════════════════╗');
@@ -73,6 +73,7 @@ class VersionCommand extends Command
 }
 
 //Changelog
+//1.8.0 - feat: modal dialog support added with showConfirm, showAlert and showDanger JS functions + used in various places for better UX
 //1.8.0 - feat: invitation and referral system added with invitation_links and invitation_referrals tables + invitation management UI in dashboard
 //1.7.1 - feat: add configurable sidebar menu items with icon support using $adminMenuItems, $commonMenuItems and $userMenuItems in config/menu.php
 //1.7.0 - Sidebar color customization support via TYRO_DASHBOARD_SIDEBAR_BG and TYRO_DASHBOARD_SIDEBAR_TEXT env variables. Sidebar Example pages and routes can be hidden using TYRO_DASHBOARD_DISABLE_EXAMPLES 
