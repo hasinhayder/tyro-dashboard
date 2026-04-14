45f3e95b53d9fd5d16f58f6cc32bb31452abf96e
Hasin Hayder <countdraculla@gmail.com>
2026-04-15 00:40:53 +0530

    chore: improved update command for supporting toast notification flash message

--- src/Console/Commands/UpdateCommand.php
+++ src/Console/Commands/UpdateCommand.php
@@ -21,6 +21,10 @@ class UpdateCommand extends Command {
             resource_path('views/vendor/tyro-dashboard/partials/user-sidebar.blade.php')
         );
 
+        $this->updatePublishedFlashMessages(
+            resource_path('views/vendor/tyro-dashboard/partials/flash-messages.blade.php')
+        );
+
         return self::SUCCESS;
     }
 
@@ -45,5 +49,19 @@ class UpdateCommand extends Command {
         if ($updatedContent !== $content) {
             file_put_contents($sidebarPath, $updatedContent);
         }
+    }
+
+    protected function updatePublishedFlashMessages(string $flashMessagesPath): void {
+        if (! file_exists($flashMessagesPath)) {
+            return;
+        }
+
+        $updatedContent = @file_get_contents(__DIR__ . '/../../../resources/views/partials/flash-messages.blade.php');
+
+        if ($updatedContent === false || $updatedContent === '') {
+            return;
+        }
+
+        file_put_contents($flashMessagesPath, $updatedContent);
     }
 }
