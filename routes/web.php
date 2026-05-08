3e7a3dc7e3b597ad5a9e5b4dc9ac1216167f318f
Hasin Hayder <countdraculla@gmail.com>
2026-05-08 20:40:31 +0530

    Route Fix

--- routes/web.php
+++ routes/web.php
@@ -107,32 +107,28 @@ Route::middleware('tyro-dashboard.admin')->group(function () {
     });
 
     // Role Management
-    if (config('tyro-dashboard.features.show_roles_menu', true)) {
-        Route::prefix('roles')->name('roles.')->group(function () {
-            Route::get('/', [RoleController::class, 'index'])->name('index');
-            Route::get('/create', [RoleController::class, 'create'])->name('create');
-            Route::post('/', [RoleController::class, 'store'])->name('store');
-            Route::get('/{id}', [RoleController::class, 'show'])->name('show');
-            Route::get('/{id}/edit', [RoleController::class, 'edit'])->name('edit');
-            Route::put('/{id}', [RoleController::class, 'update'])->name('update');
-            Route::delete('/{id}', [RoleController::class, 'destroy'])->name('destroy');
-            Route::delete('/{id}/users/{userId}', [RoleController::class, 'removeUser'])->name('remove-user');
-        });
-    }
+    Route::prefix('roles')->name('roles.')->group(function () {
+        Route::get('/', [RoleController::class, 'index'])->name('index');
+        Route::get('/create', [RoleController::class, 'create'])->name('create');
+        Route::post('/', [RoleController::class, 'store'])->name('store');
+        Route::get('/{id}', [RoleController::class, 'show'])->name('show');
+        Route::get('/{id}/edit', [RoleController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [RoleController::class, 'update'])->name('update');
+        Route::delete('/{id}', [RoleController::class, 'destroy'])->name('destroy');
+        Route::delete('/{id}/users/{userId}', [RoleController::class, 'removeUser'])->name('remove-user');
+    });
 
     // Privilege Management
-    if (config('tyro-dashboard.features.show_privileges_menu', true)) {
-        Route::prefix('privileges')->name('privileges.')->group(function () {
-            Route::get('/', [PrivilegeController::class, 'index'])->name('index');
-            Route::get('/create', [PrivilegeController::class, 'create'])->name('create');
-            Route::post('/', [PrivilegeController::class, 'store'])->name('store');
-            Route::get('/{id}', [PrivilegeController::class, 'show'])->name('show');
-            Route::get('/{id}/edit', [PrivilegeController::class, 'edit'])->name('edit');
-            Route::put('/{id}', [PrivilegeController::class, 'update'])->name('update');
-            Route::delete('/{id}', [PrivilegeController::class, 'destroy'])->name('destroy');
-            Route::delete('/{id}/roles/{roleId}', [PrivilegeController::class, 'removeRole'])->name('remove-role');
-        });
-    }
+    Route::prefix('privileges')->name('privileges.')->group(function () {
+        Route::get('/', [PrivilegeController::class, 'index'])->name('index');
+        Route::get('/create', [PrivilegeController::class, 'create'])->name('create');
+        Route::post('/', [PrivilegeController::class, 'store'])->name('store');
+        Route::get('/{id}', [PrivilegeController::class, 'show'])->name('show');
+        Route::get('/{id}/edit', [PrivilegeController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [PrivilegeController::class, 'update'])->name('update');
+        Route::delete('/{id}', [PrivilegeController::class, 'destroy'])->name('destroy');
+        Route::delete('/{id}/roles/{roleId}', [PrivilegeController::class, 'removeRole'])->name('remove-role');
+    });
 
     // Invitation Management (Admin)
     if (config('tyro-dashboard.features.invitation_system', true)) {
@@ -165,14 +161,12 @@ Route::middleware('tyro-dashboard.admin')->group(function () {
 
 // Dynamic Resources (outside admin middleware - handles its own access control)
 // This route group handles all dynamic resources defined in config or via HasCrud trait
-if (config('tyro-dashboard.features.show_resources_menu', true)) {
-    Route::prefix('resources/{resource}')->name('resources.')->group(function () {
-        Route::get('/', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'index'])->name('index');
-        Route::get('/create', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'create'])->name('create');
-        Route::post('/', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'store'])->name('store');
-        Route::get('/{id}', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'show'])->name('show');
-        Route::get('/{id}/edit', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'edit'])->name('edit');
-        Route::put('/{id}', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'update'])->name('update');
-        Route::delete('/{id}', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'destroy'])->name('destroy');
-    });
-}
+Route::prefix('resources/{resource}')->name('resources.')->group(function () {
+    Route::get('/', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'index'])->name('index');
+    Route::get('/create', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'create'])->name('create');
+    Route::post('/', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'store'])->name('store');
+    Route::get('/{id}', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'show'])->name('show');
+    Route::get('/{id}/edit', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'edit'])->name('edit');
+    Route::put('/{id}', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'update'])->name('update');
+    Route::delete('/{id}', [\HasinHayder\TyroDashboard\Http\Controllers\ResourceController::class, 'destroy'])->name('destroy');
+});
